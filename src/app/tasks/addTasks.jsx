89041a6df8b8fbf3b4dc124c482347835fbf94cd
Gabriel Pereira <gabs.pereira0@icloud.com>
2026-08-27 22:09:52 -0300

    adiciona estrutura base da página addTask

--- src/app/tasks/addTasks.jsx
+++ src/app/tasks/addTasks.jsx
@@ -1,10 +1,19 @@
-import { Text } from 'react-native';
+import { Pressable, Text, View, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { IconSave } from '../../../components/Icons/Icons';
 
 export default function AddTasks() {
   return (
     <SafeAreaView>
-      <Text>addTasks</Text>
+      <Text>Adicionar uma tarefa:</Text>
+      <View>
+        <Text>Em que você está trabalhando?</Text>
+        <TextInput />
+        <Pressable>
+          <IconSave />
+          <Text> Salvar</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
