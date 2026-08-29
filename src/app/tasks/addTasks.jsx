a872f19a9778b6ea2acc6787c2765c5b9741b647
Gabriel Pereira <gabs.pereira0@icloud.com>
2026-08-29 18:30:05 -0300

    adiciona função para mostrar e ocultar teclado com TouchableWithoutFeedback

--- src/app/tasks/addTasks.jsx
+++ src/app/tasks/addTasks.jsx
@@ -1,20 +1,37 @@
-import { Pressable, Text, View, TextInput, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  Pressable,
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+
 import { IconSave } from '../../../components/Icons/Icons';
 
 export default function AddTasks() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Adicionar uma tarefa:</Text>
-      <View style={styles.inner}>
-        <Text style={styles.label}>Em que você está trabalhando?</Text>
-        <TextInput style={styles.input} numberOfLines={10} />
-        <Pressable style={styles.button}>
-          <IconSave />
-          <Text> Salvar</Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.text}>Adicionar uma tarefa:</Text>
+          <Text style={styles.label}>Em que você está trabalhando?</Text>
+          <TextInput style={styles.input} numberOfLines={10} multiline={true} />
+          <View style={styles.actions}>
+            <Pressable style={styles.button}>
+              <IconSave />
+              <Text> Salvar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -24,6 +41,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#021123',
     gap: 16,
     alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     color: '#fff',
@@ -45,10 +63,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 16,
     borderRadius: 8,
+    height: 100,
   },
   button: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 4,
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
   },
 });
