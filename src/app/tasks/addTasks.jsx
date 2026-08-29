79a028530ca56008da1303b88d8e2e3070d375cd
Gabriel Pereira <gabs.pereira0@icloud.com>
2026-08-29 17:41:29 -0300

    realizado estilizacao dos componentes pagina addTasks

--- src/app/tasks/addTasks.jsx
+++ src/app/tasks/addTasks.jsx
@@ -1,15 +1,15 @@
-import { Pressable, Text, View, TextInput } from 'react-native';
+import { Pressable, Text, View, TextInput, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { IconSave } from '../../../components/Icons/Icons';
 
 export default function AddTasks() {
   return (
-    <SafeAreaView>
-      <Text>Adicionar uma tarefa:</Text>
-      <View>
-        <Text>Em que você está trabalhando?</Text>
-        <TextInput />
-        <Pressable>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Adicionar uma tarefa:</Text>
+      <View style={styles.inner}>
+        <Text style={styles.label}>Em que você está trabalhando?</Text>
+        <TextInput style={styles.input} numberOfLines={10} />
+        <Pressable style={styles.button}>
           <IconSave />
           <Text> Salvar</Text>
         </Pressable>
@@ -17,3 +17,38 @@ export default function AddTasks() {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#021123',
+    gap: 16,
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 26,
+  },
+  inner: {
+    backgroundColor: '#98A0A8',
+    width: '90%',
+    borderRadius: 8,
+    padding: 16,
+    gap: 32,
+  },
+  label: {
+    fontWeight: 600,
+    fontSize: 18,
+  },
+  input: {
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+  },
+});
